51c5ea8b4e642a5211f078d67888f4b4ce7cb1bc
ReDay Zarra <redayzarra@gmail.com>
2022-12-28 21:58:55 -0500

    Starting sample TensorFlow code and understanding the implementations

---
+++ Getting_Started/Sample_TF.py
@@ -0,0 +1,7 @@
+"""
+This is just a sample TensorFlow code that we will be using in the course to 
+build machine learning models. TensorFlow is a Python interface to build custom
+neural networks, it runs custom C++ code under the hood.
+
+Imorting TensorFlow is the first step of the process
+"""
